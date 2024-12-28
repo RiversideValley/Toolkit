2813a4260f1a48e29b9ea3d8047d13e3f98860c7
Lamparter <71598437+Lamparter@users.noreply.github.com>
2024-12-28 15:53:29 +0000

    Convert Alloy test project into UWP .NET 9 project

--- eng/Riverside.Alloy.PublishingServices/Class1.cs
+++ eng/Riverside.Alloy.PublishingServices/Class1.cs
@@ -1,7 +1,12 @@
-﻿namespace Riverside.Alloy.PublishingServices
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Riverside.Alloy.PublishingServices
 {
     public class Class1
     {
-
     }
 }
