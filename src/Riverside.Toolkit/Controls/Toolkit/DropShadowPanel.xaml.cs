3b03480b7787d6ac038ce67b7eb56dd06b786e32
Lamparter <71598437+Lamparter@users.noreply.github.com>
2025-01-04 12:42:47 +0000

    Make more using directives agnostic

--- src/Riverside.Toolkit/Controls/Toolkit/DropShadowPanel.xaml.cs
+++ src/Riverside.Toolkit/Controls/Toolkit/DropShadowPanel.xaml.cs
@@ -6,17 +6,6 @@ using System.Numerics;
 using System.Runtime.InteropServices.WindowsRuntime;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
-using Microsoft.UI;
-using Microsoft.UI.Composition;
-using Microsoft.UI.Xaml;
-using Microsoft.UI.Xaml.Controls;
-using Microsoft.UI.Xaml.Controls.Primitives;
-using Microsoft.UI.Xaml.Data;
-using Microsoft.UI.Xaml.Hosting;
-using Microsoft.UI.Xaml.Input;
-using Microsoft.UI.Xaml.Media;
-using Microsoft.UI.Xaml.Navigation;
-using Microsoft.UI.Xaml.Shapes;
 using Windows.UI;
 
 // The User Control item template is documented at https://go.microsoft.com/fwlink/?LinkId=234236
