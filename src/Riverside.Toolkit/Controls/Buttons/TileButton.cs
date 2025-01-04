832124a03382de2426e2615db1608821c68b3bcf
Lamparter <71598437+Lamparter@users.noreply.github.com>
2025-01-04 12:26:11 +0000

    Even more global usings

--- src/Riverside.Toolkit/Controls/Buttons/TileButton.cs
+++ src/Riverside.Toolkit/Controls/Buttons/TileButton.cs
@@ -4,15 +4,6 @@ using Riverside.Toolkit.Icons;
 using System;
 using System.Threading.Tasks;
 using System.Xml.Linq;
-using Microsoft.UI.Composition;
-using Microsoft.UI.Xaml;
-using Microsoft.UI.Xaml.Controls;
-using Microsoft.UI.Xaml.Data;
-using Microsoft.UI.Xaml.Documents;
-using Microsoft.UI.Xaml.Hosting;
-using Microsoft.UI.Xaml.Input;
-using Microsoft.UI.Xaml.Media;
-using Microsoft.UI.Xaml.Media.Animation;
 
 // The Templated Control item template is documented at https://go.microsoft.com/fwlink/?LinkId=234235
 
