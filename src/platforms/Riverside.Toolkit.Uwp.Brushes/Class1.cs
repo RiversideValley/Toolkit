f34a6f7c03354b0ecb4929bd07a25ba18686f1f5
Lamparter <71598437+Lamparter@users.noreply.github.com>
2025-01-12 09:05:48 +0000

    Create all project heads

---
+++ src/platforms/Riverside.Toolkit.Uwp.Brushes/Class1.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Riverside.Toolkit.Uwp.Brushes;
+public class Class1
+{
+}
