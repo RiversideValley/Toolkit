d73d1d6399f2c817301247ceb184745924dd74d7
Lamparter <71598437+Lamparter@users.noreply.github.com>
2025-01-22 18:22:08 +0000

    Tooling test 🧪👀

---
+++ tests/dceng/Riverside.Toolkit._CoreExample/ExampleClass.cs
@@ -0,0 +1,21 @@
+// This is an example class, used to test the custom tooling.
+
+using System;
+
+namespace Riverside.Toolkit._CoreExample;
+
+/// <summary>
+/// This is an example class, used to test the custom tooling.
+/// </summary>
+/// <remarks>
+/// If you are seeing this, the tooling is working!
+/// </remarks>
+public class ExampleClass
+{
+}
+
+#if THIS_IS_WINUI
+public class WinUiClass
+{
+}
+#endif
---
+++ tests/dceng/Riverside.Toolkit._PlatformExample.Uwp/Class1.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Riverside.Toolkit._PlatformExample.Uwp;
+public class Class1
+{
+}
