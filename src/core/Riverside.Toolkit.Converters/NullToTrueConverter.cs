2aa610b6ad08ae67c92674b26166c691d78ec403
Lamparter <71598437+Lamparter@users.noreply.github.com>
2025-02-05 07:26:46 +0000

    Add more converters

---
+++ src/core/Riverside.Toolkit.Converters/NullToTrueConverter.cs
@@ -0,0 +1,29 @@
+namespace Riverside.Toolkit.Converters;
+
+public partial class NullToTrueConverter
+    : ValueConverter<object?, bool>
+{
+    /// <summary>
+    /// Determines whether an inverse conversion should take place.
+    /// </summary>
+    /// <remarks>If set, the value <see langword="true"/> results in <see cref="Visibility.Collapsed"/>, and <see langword="false"/> in <see cref="Visibility.Visible"/>.</remarks>
+    public bool Inverse { get; set; }
+
+    /// <summary>
+    /// Converts a source value to the target type.
+    /// </summary>
+    /// <param name="value"></param>
+    /// <param name="parameter"></param>
+    /// <param name="language"></param>
+    /// <returns></returns>
+    protected override bool Convert(object? value, object? parameter, string? language) => this.Inverse ? value != null : value == null;
+
+    /// <summary>
+    /// Converts a target value back to the source type.
+    /// </summary>
+    /// <param name="value"></param>
+    /// <param name="parameter"></param>
+    /// <param name="language"></param>
+    /// <returns></returns>
+    protected override object? ConvertBack(bool value, object? parameter, string? language) => null;
+}
---
+++ src/core/Riverside.Toolkit.Converters/StringNullOrWhiteSpaceToTrueConverter.cs
@@ -0,0 +1,29 @@
+﻿namespace Riverside.Toolkit.Converters;
+
+public partial class StringNullOrWhiteSpaceToTrueConverter
+    : ValueConverter<string, bool>
+{
+    /// <summary>
+    /// Determines whether an inverse conversion should take place.
+    /// </summary>
+    /// <remarks>If set, the value True results in <see cref="Visibility.Collapsed"/>, and false in <see cref="Visibility.Visible"/>.</remarks>
+    public bool Inverse { get; set; }
+
+    /// <summary>
+    /// Converts a source value to the target type.
+    /// </summary>
+    /// <param name="value"></param>
+    /// <param name="parameter"></param>
+    /// <param name="language"></param>
+    /// <returns></returns>
+    protected override bool Convert(string? value, object? parameter, string? language) => this.Inverse ? !string.IsNullOrWhiteSpace(value) : string.IsNullOrWhiteSpace(value);
+
+    /// <summary>
+    /// Converts a target value back to the source type.
+    /// </summary>
+    /// <param name="value"></param>
+    /// <param name="parameter"></param>
+    /// <param name="language"></param>
+    /// <returns></returns>
+    protected override string ConvertBack(bool value, object? parameter, string? language) => string.Empty;
+}
