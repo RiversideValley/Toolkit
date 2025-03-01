3fee7a31030b1fd8759faa9f77b600e610bf3103
Lamparter <71598437+Lamparter@users.noreply.github.com>
2025-03-01 15:04:42 +0000

    Create `Riverside.Extensions.Accountability`

---
+++ src/extensions/Riverside.Extensions.Accountability/NotMyCodeAttribute.cs
@@ -0,0 +1,19 @@
+﻿namespace Riverside.Extensions.Accountability;
+
+[AttributeUsage(AttributeTargets.All)]
+public class NotMyCodeAttribute : Attribute
+{
+    /// <summary>
+    /// Generic indication that the code is not owned by you.
+    /// </summary>
+    public NotMyCodeAttribute()
+    {
+    }
+/*
+    public NotMyCodeAttribute(Spdx license)
+    {
+        License = license;
+    }*/
+
+    public string License { get; }
+}
