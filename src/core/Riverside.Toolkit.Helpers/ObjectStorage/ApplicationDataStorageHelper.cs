80058cf6ee5d098cc4fc7f33f7430767cf290d9f
Lamparter <71598437+Lamparter@users.noreply.github.com>
2025-03-15 11:24:51 +0000

    Remove license headers

--- src/core/Riverside.Toolkit.Helpers/ObjectStorage/ApplicationDataStorageHelper.cs
+++ src/core/Riverside.Toolkit.Helpers/ObjectStorage/ApplicationDataStorageHelper.cs
@@ -1,9 +1,5 @@
 #if !Wpf
 
-// Licensed to the .NET Foundation under one or more agreements.
-// The .NET Foundation licenses this file to you under the MIT license.
-// See the LICENSE file in the project root for more information.
-
 using CommunityToolkit.Common.Helpers;
 using CommunityToolkit.Helpers;
 using Riverside.Extensions.Accountability;
